450f00a6f2cfb826b24cbd2242187b892eee9e32
Danilo Araujo <daniloaraujo@Danilos-MacBook-Pro.local>
2021-07-01 16:03:21 -0300

    minor changes

--- ch1/PalindromePermutation.py
+++ ch1/PalindromePermutation.py
@@ -1,21 +1,20 @@
 from collections import Counter
 import unittest
 
-def pal_perm(string):
-    counter = Counter()
-    i = 0
-    string = string.lower()
-    for char in string:
-        if ord(char) >= 97 and ord(char) <= 122:
-            counter[char] += 1
-            i += 1
-    i = i%2
-    for key in counter:
-        if counter[key]%2 != 0 and i==0:
-            return False
-        elif counter[key]%2 != 0 and i==1:
-            i = 0
-    return True
+def pal_perm(s: str):
+    fs = "".join(filter(lambda x: x >= "a" and x <= "z",s.lower()))
+    counter = Counter(fs)
+    odds = 0
+    is_pal = True
+
+    for count in counter.values():
+        if count%2 == 1:
+            odds += 1
+
+    if odds > 1:
+        is_pal = False
+
+    return is_pal
 
 class Test(unittest.TestCase):
     '''Test Cases'''
@@ -27,7 +26,7 @@ class Test(unittest.TestCase):
         ('Random Words', False),
         ('Not a Palindrome', False),
         ('no x in nixon', True),
-        ('azAZ', True)]
+        ('aziZ', False)]
 
     def test_pal_perm(self):
         for [test_string, expected] in self.data:
